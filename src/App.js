2a52c403fb248828a7d6a2d2c127c88872d4045c
Yogender Kumar Singh <125579992+theyogender@users.noreply.github.com>
2024-02-22 23:06:42 +0530

    News API Project

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// import logo from "./logo.svg";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Text from "./components/Text";
+import Alert from "./components/Alert";
+import React, { useState } from "react";
+import About from "./components/About";
+
+
+
+
+
+function App() {
+
+   
+  const [mode, setMode] = useState('light');
+  const [alert, Setalert] = useState('null');
+  let showalert = (msg, type) => {
+    Setalert({
+      message: msg,
+      typ: type
+    })
+
+    setTimeout(() => {
+      Setalert('null')
+    }, 1500)
+
+  }
+  let chgbg = () => {
+    if (mode === 'light') {
+      setMode('black')
+      document.body.style.backgroundColor = '#2F4F4F';
+      showalert('Dark mode Enabled', 'danger')
+      document.title = 'Gujjar Edits'
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showalert('Light mode Enabled', 'info')
+    }
+  }
+
+  return (
+
+      <>
+      <Navbar  changebg={chgbg}/>
+      <Alert alert={alert} />
+      </>
+      
+  );
+}
+
+export default App;
